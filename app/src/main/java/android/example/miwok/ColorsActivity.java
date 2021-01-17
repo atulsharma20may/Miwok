fbccbe753cbe9c68d5924be7f1f7bc3386665551
Atul Sharma <atulsharma20may@gmail.com>
2021-01-17 13:25:14 +0530

    Add: all dictionary words in

--- app/src/main/java/android/example/miwok/ColorsActivity.java
+++ app/src/main/java/android/example/miwok/ColorsActivity.java
@@ -3,6 +3,9 @@ package android.example.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        ArrayList<Word> words = new ArrayList<>();
+
+        words.add(new Word("weṭeṭṭi", "red"));
+        words.add(new Word("chokokki", "green"));
+        words.add(new Word("ṭakaakki", "brown"));
+        words.add(new Word("ṭopoppi", "grey"));
+        words.add(new Word("kululli", "black"));
+        words.add(new Word("kelelli", "white"));
+        words.add(new Word("ṭopiisә", "dusty yellow"));
+        words.add(new Word("chiwiiṭә", "mustard yellow"));
+
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
--- app/src/main/java/android/example/miwok/FamilyActivity.java
+++ app/src/main/java/android/example/miwok/FamilyActivity.java
@@ -3,6 +3,9 @@ package android.example.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+
+        ArrayList<Word> words = new ArrayList<>();
+
+        words.add(new Word("әpә", "father"));
+        words.add(new Word("әṭa", "mother"));
+        words.add(new Word("angsi", "son"));
+        words.add(new Word("tune", "daughter"));
+        words.add(new Word("taachi", "older brother"));
+        words.add(new Word("chalitti", "younger brother"));
+        words.add(new Word("teṭe", "older sister"));
+        words.add(new Word("kolliti", "younger sister"));
+        words.add(new Word("ama", "grand mother"));
+        words.add(new Word("paapa", "grand father"));
+
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
